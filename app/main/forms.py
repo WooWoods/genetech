6efc1ec866268de84c5dc554785ce81da04672d8
Woods <wujunjames@126.com>
2017-12-18 20:48:03 +0800

    fix form bugs

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, DateField
 from wtforms.validators import Required, Length
@@ -7,15 +7,15 @@ from wtforms.validators import Required, Length
 
 class OrderForm(FlaskForm):
     username = StringField('user name', validators=[Required()])
-    gender = SelectField('Gender', choices=[('male', '男'), ('female', '女')])
+    gender = SelectField('Gender', choices=[('男', '男'), ('女', '女')], validators=[Required()])
     ID = StringField('identification NO.', validators=[Required()])
-    report_type = StringField('report type, gene', validators=[Required()])
-    genotype = StringField('genotype', validators=[Required()])
-    test_time = DateField('Testing date', format='%Y-%m-%d')
+    report_type = StringField('report type, gene', default='卫士基因检测-I',validators=[Required()])
+    genotype = StringField('genotype', default='+/-',validators=[Required()])
+    test_time = DateField('Testing date', format='%Y-%m-%d', default=datetime.utcnow() - timedelta(days=5))
     report_num = StringField('report number', validators=[Required()])
-    report_danwei = StringField('report institution', validators=[Required()])
-    inspect_danwei = StringField('inspection institution', validators=[Required()])
+    report_danwei = StringField('report institution', default='中科院团队上海基因检测实验室',validators=[Required()])
+    inspect_danwei = StringField('inspection institution', default='/',validators=[Required()])
     report_time = DateField('Testing date', format='%Y-%m-%d', default=datetime.utcnow)
-    submit = SubmitField('submit')
+    submit = SubmitField('Submit')
 
     
